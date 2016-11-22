f42ec9591e3d8ec53b882ffa8a91fe8a87c90207
Kirk Rohani <kirk.rohani@gmail.com>
2016-11-21 21:35:11 -0600

    Created Main Controller servlet and redirected to index.jsp

---
+++ src/meritocracy/roombook/core/MainController.java
@@ -0,0 +1,42 @@
+package meritocracy.roombook.core;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainController
+ */
+@WebServlet(urlPatterns = { "/Lobby" })
+public class MainController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+
+    public MainController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Inside of Main Controller Servlet doGet() method with context " + request.getContextPath());
+		
+		//Forward to index.jsp landing page initially when user first arrives
+		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
