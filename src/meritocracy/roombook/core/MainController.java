899d7d84688ff9e5c5cc0b06beb04611bc12185e
Kirk Rohani <kirk.rohani@gmail.com>
2016-11-22 23:43:35 -0600

    Added logging with slf4j and log4j

--- src/meritocracy/roombook/core/MainController.java
+++ src/meritocracy/roombook/core/MainController.java
@@ -1,11 +1,17 @@
 package meritocracy.roombook.core;
 
 import java.io.IOException;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * Servlet implementation class MainController
@@ -13,22 +19,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(urlPatterns = { "/Lobby" })
 public class MainController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
        
 
     public MainController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		System.out.println("Inside of Main Controller Servlet doGet() method with context " + request.getContextPath());
+		ServletContext servletContext = getServletContext();
+		final String DB_PROPERTIES_FILE = servletContext.getInitParameter("DatabasePropertiesFile");
 		
-		//Forward to index.jsp landing page initially when user first arrives
-		getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
+		logger.info("Inside of Main Controller Servlet doGet() method with context {}", request.getContextPath());
+		logger.info("Retrieved database properties file {}", DB_PROPERTIES_FILE); 
+		
+		//Forward to index.jsp
+		servletContext.getRequestDispatcher("/index.jsp").forward(request, response);
 	}
 
 	/**
